7ca6379c03c4330b5e150aabd3af6de2c217d4c8
lee-jihyoun <lee.jihyoun7@gmail.com>
2025-04-10 17:47:40 +0900

    2025.04.10
    env 설정

--- speech-service/speech-to-text.py
+++ speech-service/speech-to-text.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-speech_key = os.getenv('SPEECH_KEY')
-service_region = os.getenv('SPEECH_REGION')
-endpoint = os.getenv('SPEECH_ENDPOINT')  # 필요 시 사용
+speech_key = os.getenv('AI_SERVICE_KEY')
+service_region = os.getenv('AI_SERVICE_REGION')
+endpoint = os.getenv('AI_SERVICE_ENDPOINT')  # 필요 시 사용
+
+print(speech_key, service_region, endpoint)
 
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 # endpoint가 별도로 필요 없는 경우도 있지만, 특정 Custom Speech 등의 시나리오에서는 endpoint 지정 가능
