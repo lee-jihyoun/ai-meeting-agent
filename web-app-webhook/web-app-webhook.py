993d4d352122d6231b17475e1306c8f2cd470477
lee-jihyoun <lee.jihyoun7@gmail.com>
2025-05-13 11:17:47 +0900

    2025.05.13 - 녹음 기능 개발 완료 - Flask 통신 기능 테스트 필요 - Wav 파일 저장경로 변경 필요

---
+++ web-app-webhook/web-app-webhook.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('recording.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook_handler():
+    # 요청 데이터(JSON)를 가져옴
+    data = request.get_json()
+
+    # 예외 처리: 데이터가 없거나 "action"이 없을 경우 오류 반환
+    if not data or "action" not in data:
+        return jsonify({"error": "Missing action parameter"}), 400
+
+    # action 값을 가져옴
+    action = data.get("action")
+    email = data.get("email")
+    attendees = data.get("attendees")
+
+    # action 값에 따라 분기 처리
+    if action == "startMeeting":
+        return process_start_meeting(email, attendees)
+    elif action == "endMeeting":
+        return process_end_meeting(email)
+    else:
+        return jsonify({"error": "Invalid action type"}), 400
+
+def process_start_meeting(email, attendees):
+    return jsonify({"status": "startMeeting", "email": email, "attendees" : attendees})
+
+def process_end_meeting(email):
+    return jsonify({"status": "endMeeting", "email": email})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='9090')
