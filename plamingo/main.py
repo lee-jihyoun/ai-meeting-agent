502c2d22dc29e5d13cb4d8ce202cf96f6daeda6c
yeyoung8805 <yyk8805@naver.com>
2025-05-29 10:43:03 +0900

    [SSL 인증서] main.py ssl_context 수정

--- plamingo/main.py
+++ plamingo/main.py
@@ -72,4 +72,5 @@ def webhook_handler():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='9090')
+    # app.run(host='0.0.0.0', port='9090')
+    app.run(host='0.0.0.0', port='443', ssl_context=('cert.pem', 'key.pem'))
